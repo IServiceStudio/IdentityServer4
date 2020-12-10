bf67e7e076b03a1c1cb9c4a4334e4559a328754b
LvFeng <lv.feng@outlook.com>
2020-12-10 15:57:33 +0800

    1、授权Cookie写入到服务端 2、添加Cookie授权事件

--- IdentityServer4Solution/AuthDemo3/Startup.cs
+++ IdentityServer4Solution/AuthDemo3/Startup.cs
@@ -1,9 +1,12 @@
+using AuthDemo3.Utility;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
 
 namespace AuthDemo3
 {
@@ -20,13 +23,26 @@ namespace AuthDemo3
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddMemoryCache();
+            services.AddScoped<ITicketStore, MemoryCacheTicketStore>();
             services.AddAuthentication(options =>
             {
                 options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
                 options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
                 options.DefaultSignOutScheme = CookieAuthenticationDefaults.AuthenticationScheme;
                 options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-            }).AddCookie();
+            }).AddCookie(options =>
+            {
+                //cookie写入到服务端
+                options.SessionStore = services.BuildServiceProvider().GetService<ITicketStore>();
+
+                options.Events = new CookieAuthenticationEvents()
+                {
+                    OnSignedIn = async context => { Console.WriteLine("OnSignedIn"); await Task.CompletedTask; },
+                    OnSigningIn = async context => { Console.WriteLine("OnSigningIn"); await Task.CompletedTask; },
+                    OnSigningOut = async context => { Console.WriteLine("OnSigningOut"); await Task.CompletedTask; }
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ IdentityServer4Solution/AuthDemo3/Utility/MemoryCacheTicketStore.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthDemo3.Utility
+{
+    public class MemoryCacheTicketStore : ITicketStore
+    {
+        private readonly IMemoryCache memoryCache;
+        private const string KeyPrefix = "IService-";
+
+        public MemoryCacheTicketStore(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+        }
+
+        public Task RemoveAsync(string key)
+        {
+            memoryCache.Remove(key);
+            return Task.CompletedTask;
+        }
+
+        public Task RenewAsync(string key, AuthenticationTicket ticket)
+        {
+            var options = new MemoryCacheEntryOptions();
+            var expiresUtc = ticket.Properties.ExpiresUtc;
+            if (expiresUtc.HasValue)
+            {
+                options.SetAbsoluteExpiration(expiresUtc.Value);
+            }
+            options.SetSlidingExpiration(TimeSpan.FromHours(1));
+            memoryCache.Set(key, ticket, options);
+            return Task.CompletedTask;
+        }
+
+        public Task<AuthenticationTicket> RetrieveAsync(string key)
+        {
+            memoryCache.TryGetValue(key, out AuthenticationTicket ticket);
+            return Task.FromResult(ticket);
+        }
+
+        public async Task<string> StoreAsync(AuthenticationTicket ticket)
+        {
+            var key = KeyPrefix + Guid.NewGuid().ToString("N");
+            await RenewAsync(key, ticket);
+            return key;
+        }
+    }
+}
